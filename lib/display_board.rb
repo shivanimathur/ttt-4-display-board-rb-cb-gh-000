89d4ef4199766ba3809d3a23817bdac06c540a37
Shivani Mathur <qwellery@gmail.com>
2017-12-25 08:39:03 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,7 +2,7 @@
 # out the current state.
 board = ["   ","   ","   ","   ","   ","   ","   ","   ","   "]
 def display_board(rows)
-  puts "#{rows[0]} | #{rows[1]} | #{rows[2]}"
+  puts "#{rows[0]}|#{rows[1]}|#{rows[2]}"
   puts "-----------"
   puts "#{rows[3]}|#{rows[4]}|#{rows[5]}"
   puts "-----------"
@@ -10,5 +10,5 @@ def display_board(rows)
 end
 board = ["   ","   ","   ","   ","   ","   ","   ","   ","   "]
 display_board(board)
-board = [" O ","   ","   ","   ","   ","   ","   ","   ","   "]
+board = ["O","   ","   ","   ","   ","   ","   ","   ","   "]
 display_board(board)
