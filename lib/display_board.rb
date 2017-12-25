0c0a1826f7345f22b44f5a72b492828b8c4d6a5a
Shivani Mathur <qwellery@gmail.com>
2017-12-25 08:26:14 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -9,7 +9,7 @@ def display_board(board)
   puts "#{board[6]}|#{board[7]}|#{board[8]}"
 end
 display_board(board)
-board[4] = " X "
+board = [" O ","   ","   ","   ","   ","   ","   ","   ","   "]
 display_board(board)
 board[0] = " O "
 board[4] = "   "
